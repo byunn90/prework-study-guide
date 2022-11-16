48c8d89b8f546f43a20d444874a3bd991a8edb3c
byunn90 <kayhanturk90@Live.com>
2022-11-17 04:15:59 +1100

    added JavaScript var random topic

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,32 @@
 const shape = "circle";
-const topics = "HTML, CSS, Git, JavaScript";
+const topics = ["HTML", "CSS", "Git", "JavaScript"];
 const topic = "HTML";
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+// for (let x = 0; x < topics.length; x++) {
+//   console.log(topics[x]);
 
-if (topic === "HTML") {
-  console.log("lets study HTML");
-} else if (topic === "CSS") {
-  console.log("Lets study CSS!");
-} else if (topic === "Git") {
-  console.log("Lets study Git!");
-} else if (topic === "JavaScript") {
-  console.log("lets study JavaScript");
-} else {
-  console.log("Please try again!");
+function listTopics() {
+  for (x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+  }
 }
+
+function selectTopics(input) {
+  if (topic === "HTML") {
+    console.log("lets study HTML");
+  } else if (topic === "CSS") {
+    console.log("Lets study CSS!");
+  } else if (topic === "Git") {
+    console.log("Lets study Git!");
+  } else if (topic === "JavaScript") {
+    console.log("lets study JavaScript");
+  } else {
+    console.log("Please try again!");
+  }
+}
+function helloWorld() {
+  console.log("Hello, world I am a function");
+}
+selectTopics("HTML");
+helloWorld();
+listTopics();
