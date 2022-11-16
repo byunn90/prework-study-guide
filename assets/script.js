e558321ca31dab92088eadb51dc983e425eeed63
byunn90 <kayhanturk90@Live.com>
2022-11-17 02:25:17 +1100

    javascripy

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,15 @@
 const shape = "circle";
 const topics = "HTML, CSS, Git, JavaScript";
-console.log(topics);
-console.log(shape);
-console.log("hi");
+const topic = "HTML";
+
+if (topic === "HTML") {
+  console.log("lets study HTML");
+} else if (topic === "CSS") {
+  console.log("Lets study CSS!");
+} else if (topic === "Git") {
+  console.log("Lets study Git!");
+} else if (topic === "JavaScript") {
+  console.log("lets study JavaScript");
+} else {
+  console.log("Please try again!");
+}
